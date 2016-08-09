4973c740bfa4a5c48eb17a3817fd8bf0753195e6
victoria tapia <victoria_tapia@discovery.com>
2016-08-09 15:10:40 -0700

    complete first python drill and comments exercise.

--- ex1.py
+++ ex1.py
@@ -5,3 +5,7 @@ print "This is fun."
 print "Yay! Printing."
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+
+# Study Drills
+print "print another line for this drill, but not the next line of text."
+# print "Don't print this line."
